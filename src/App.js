5df834fff2c77ffa85af0dc961f163ff7f42b46e
hofar <hofar.ebook@gmail.com>
2023-07-03 18:00:55 +0700

    Revert "update view"
    This reverts commit 31be60789f152088f6a6a28fdc28fcc5e36150d2.

--- src/App.js
+++ src/App.js
@@ -168,18 +168,13 @@ function App() {
         <img src={logoWhite} alt="Company Logo" className="company-logo" />
         {/* Dropdown dengan teks "TECHNOLOGY DEPARTMENT" */}
         {/* ... */}
-        <div className="box">
-          <select name="dropdown" id="dropdown" title="dropdown">
-            <option value="1">TECHNOLOGY DEPARTMENT</option>
-          </select>
-          <p className="w-70">Jl. Lembong No 8 Kel. Braga Kec. Sumur Bandung, Kota Bandung, Jawa Barat</p>
-        </div>
+        <p>Jl. Lembong No 8 Kel. Braga Kec. Sumur Bandung, Kota Bandung, Jawa Barat</p>
         <nav className="footer-menu">
           {/* Menu footer yang dapat diklik */}
           {/* ... */}
-          <a href="/">Who We Are</a>
-          <a href="/">Our Values</a>
-          <a href="/">The Perks</a>
+          <a href="#">Who We Are</a>
+          <a href="#">Our Values</a>
+          <a href="#">The Perks</a>
         </nav>
       </section>
     </div>
